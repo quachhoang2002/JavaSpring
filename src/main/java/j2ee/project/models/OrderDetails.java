6d19baebb2cd79e53f079bcb2dfe1c32104e6bcc
quachhoang2002 <97105329+quachhoang2002@users.noreply.github.com>
2023-10-22 10:07:37 +0700

    complete pagination and change some response value

---
+++ src/main/java/j2ee/project/models/OrderDetails.java
@@ -0,0 +1,81 @@
+package j2ee.project.models;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "order_details")
+public class OrderDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @ManyToOne
+    private Order order;
+
+    @ManyToOne
+    private Product product;
+    private Integer quantity;
+    private double price;
+
+    public OrderDetails() {
+    }
+
+    public OrderDetails(Integer id, Order order, Product product, Integer quantity, double price) {
+        this.id = id;
+        this.order = order;
+        this.product = product;
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "OrderDetail{" +
+                "id=" + id +
+                ", order=" + order +
+                ", product=" + product +
+                ", quantity=" + quantity +
+                ", price=" + price +
+                '}';
+    }
+}
